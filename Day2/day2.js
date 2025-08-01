86b581e85ec4e95d1dd526b7fc87579d51f914cb
HuyNM139 <huy668217@gmail.com>
2025-08-01 20:23:39 +0700

    first commit

---
+++ Day2/day2.js
@@ -0,0 +1,13 @@
+let inputText = document.getElementById("inputText")
+let convertButton = document.getElementById("convertButton")
+let resultText = document.getElementById("resultText")
+let emptyString = []
+convertButton.addEventListener("click", function() {
+    console.log(inputText.value.toUpperCase());
+    let div = document.createElement("div")
+    div.innerText = inputText.value.toUpperCase()
+    resultText.appendChild(div)
+    emptyString.push(inputText.value.toUpperCase())
+    localStorage.setItem("input",JSON.stringify(emptyString))
+    inputText.value = ""
+})
